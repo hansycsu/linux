85037af6b9df5876aba5ea829d292762fc331412
YC Su <hansycsu@gmail.com>
2018-01-03 01:48:47 +0800

    Add options argument to sLinkDotFiles.sh

--- scripts/sLinkDotFiles.sh
+++ scripts/sLinkDotFiles.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+
+if [ "$1" == "mint" ]; then
+    echo "Processing mint..."
+elif [ "$1" == "cygwin" ]; then
+    echo "Processing cygwin..."
+else
+    echo "Error: Unknown argument"
+    echo "Syntax: sLinkDotFiles [mint|cygwin]"
+    exit 0
+fi
+
 SCR_PATH=$(dirname $BASH_SOURCE)
 SCR_PATH=$(cd $SCR_PATH && pwd)
 cd $SCR_PATH/../dotfiles
-rm -v ~/.bash_aliases
-rm -v ~/.bash_env
-ln -vs $(pwd)/.bash_aliases ~/
-ln -vs $(pwd)/.bash_env ~/
-ln -vs $(pwd)/.gitconfig ~/
+
+echo "Linking general files..."
+ln -fsv $(pwd)/.bash_aliases ~/
+ln -fsv $(pwd)/.bash_env ~/
+ln -fsv $(pwd)/.gitconfig ~/
+
+if [ "$1" == "mint" ]; then
+    echo "Linking mint files..."
+    ln -fsv $(pwd)/.bashrc_mint ~/.bashrc
+elif [ "$1" == "cygwin" ]; then
+    echo "Linking cygwin files..."
+    ln -fsv $(pwd)/.bashrc_cygwin ~/.bashrc
+    ln -fsv $(pwd)/.inputrc ~/
+fi
