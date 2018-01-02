f08ffeb82fd67d652998b4b0bcdf35329e932f71
YC Su <hansycsu@gmail.com>
2018-01-02 16:06:02 +0800

    Mainly update bashrc.cygwin & sLinkDotFiles.sh

--- scripts/sLinkDotFiles.sh
+++ scripts/sLinkDotFiles.sh
@@ -2,5 +2,8 @@
 SCR_PATH=$(dirname $BASH_SOURCE)
 SCR_PATH=$(cd $SCR_PATH && pwd)
 cd $SCR_PATH/../dotfiles
+rm -v ~/.bash_aliases
+rm -v ~/.bash_env
 ln -vs $(pwd)/.bash_aliases ~/
 ln -vs $(pwd)/.bash_env ~/
+ln -vs $(pwd)/.gitconfig ~/
