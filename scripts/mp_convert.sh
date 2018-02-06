ecceea4058cc55738bc9245427eb069e7e48dbb8
YC Su <hansycsu@gmail.com>
2018-02-06 17:49:45 +0800

    Update mp_convert.sh and mp_copyToVs72.sh

--- scripts/mp_convert.sh
+++ scripts/mp_convert.sh
@@ -1,34 +1,67 @@
 #!/bin/bash
 
-for file in "$@"; do
-    if [ ! -f "$file" ]; then
-        echo "File $file not exist!" 1>&2; continue;
+fileList=( 'cls_s_p_textedit_ts6edit.cpp'
+           'cls_s_p_textedit_ts6edit.h'
+           'ts/cc_ts1.cpp' 'ts/cc_ts1.h' )
+
+for file in "${fileList[@]}"; do
+  if [ ! -f "$file" ]; then
+    echo "File $file not exist!" 1>&2; continue;
+  fi
+  lineNumList=$(grep -n "// Comment for YC Su Test [{}]\{3\}" "$file" | awk -F: '{print $1}')
+  if [ -z "$lineNumList" ]; then
+    echo "Mark not found in '$file'" 1>&2; continue;
+  fi
+  echo "Processing $file ..."
+  flag=0
+  for i in $lineNumList; do
+    if [ $flag == 0 ]; then
+      tmpList=$i
+      flag=1
+    else
+      tmpList=($tmpList $i)
+      tmpList[0]=$(expr ${tmpList[0]} + 1)
+      tmpList[1]=$(expr ${tmpList[1]} - 1)
+      if [ -z "$tmpStr" ]; then
+        tmpStr=$(sed "${tmpList[0]},${tmpList[1]}s/^\(\s*\)\/\//\1/" "$file")
+      else
+        tmpStr=$(sed "${tmpList[0]},${tmpList[1]}s/^\(\s*\)\/\//\1/" <<< "$tmpStr")
+      fi
+      flag=0
     fi
-    lineNumList=$(grep -n "// Comment for YC Su Test [{}]\{3\}" "$file" | awk -F: '{print $1}')
-    if [ -z "$lineNumList" ]; then
-        echo "Mark not found in '$file'" 1>&2; continue;
-    fi
-    echo "Processing $file ..."
-    flag=0
-    for i in $lineNumList; do
-        if [ $flag == 0 ]; then
-            tmpList=$i
-            flag=1
-        else
-            tmpList=($tmpList $i)
-            tmpList[0]=$(expr ${tmpList[0]} + 1)
-            tmpList[1]=$(expr ${tmpList[1]} - 1)
-            if [ -z "$tmpStr" ]; then
-                tmpStr=$(sed "${tmpList[0]},${tmpList[1]}s/^\(\s*\)\/\//\1/" "$file")
-            else
-                tmpStr=$(sed "${tmpList[0]},${tmpList[1]}s/^\(\s*\)\/\//\1/" <<< "$tmpStr")
-            fi
-            flag=0
-        fi
-    done
-    if [ ! -d '.send' ]; then
-        mkdir '.send'
-    fi
-    echo "$tmpStr" > ".send/$file"
-    unset tmpStr
+  done
+  if [ ! -d '.send' ]; then
+    mkdir '.send'
+  fi
+  fileName=$(basename "$file")
+  echo "$tmpStr" > ".send/$fileName"
+  unset tmpStr
 done
+
+
+# Check size & Copy .send/ files to vs72
+updateVs72 () {
+  for file in "${fileList[@]}"; do
+    sum0=$(stat -c %s "$file")
+    if [ -f "$vPath/$file" ]; then
+      sum1=$(stat -c %s "$vPath/$file")
+    else
+      sum1=NULL
+    fi
+
+    if [ $sum0 == $sum1 ]; then
+      echo "$file equals!"
+    else
+      cp -auv "$file" "$vPath/"
+    fi
+  done
+}
+
+cd .send
+fileList=( 'cls_s_p_textedit_ts6edit.cpp' 'cls_s_p_textedit_ts6edit.h' )
+vPath='/cygdrive/d/imx6/vs72/vs72/scr_pg'
+updateVs72
+
+fileList=( 'cc_ts1.cpp' 'cc_ts1.h' )
+vPath='/cygdrive/d/imx6/vs72/vs72/ts'
+updateVs72
--- scripts/mp_copyToVs72.sh
+++ scripts/mp_copyToVs72.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-cd .send
-fileList=$(ls | grep -v '^clear$')
-vPath='/cygdrive/d/imx6/vs72/vs72/scr_pg'
-
-OLD_IFS=$IFS
-IFS=$'\n'
-for file in $fileList; do
+# Check size & Copy .send/ files to vs72
+updateVs72 () {
+  for file in "${fileList[@]}"; do
     sum0=$(stat -c %s "$file")
     if [ -f "$vPath/$file" ]; then
-        sum1=$(stat -c %s "$vPath/$file")
+      sum1=$(stat -c %s "$vPath/$file")
     else
-        sum1=NULL
+      sum1=NULL
     fi
 
     if [ $sum0 == $sum1 ]; then
-        echo "$file equals!"
+      echo "$file equals!"
     else
-        cp -auv "$file" "$vPath/"
-        #echo copy "$file" to "$vPath/"
+      cp -auv "$file" "$vPath/"
     fi
-done
-IFS=$OLD_IFS
+  done
+}
+
+cd .send
+fileList=( 'cls_s_p_textedit_ts6edit.cpp' 'cls_s_p_textedit_ts6edit.h' )
+vPath='/cygdrive/d/imx6/vs72/vs72/scr_pg'
+updateVs72
+
+fileList=( 'cc_ts1.cpp' 'cc_ts1.h' )
+vPath='/cygdrive/d/imx6/vs72/vs72/ts'
+updateVs72
