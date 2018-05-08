ce294eac10cf488424cb408c6161dbf230fc7fff
YC Su <hansycsu@gmail.com>
2018-05-08 10:18:29 +0800

    Use return to leave function in cgrep.sh

--- scripts/cgrep.sh
+++ scripts/cgrep.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-# Code grep : cgrep
+# Code grep: cgrep
+#
+# Only search in file, reserve colors, and format output (replace
+#
+# Usage:
+#   cgrep [PATTERN] [FILENAME]
+cgrep() {
+  if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: cgrep [PATTERN] [FILENAME]"
+    return 1
+  fi
+
+  grep --color=always -aHn "$1" "$2" |
+  sed -Ee 's/^([^:]+:[^:]+:.{6})\s*/\1\t/'
+}
+
+# Code grep all: cgrepall
 #
 # Only search in cpp and h file, reserve colors, and format output (replace
 # leading space with a tab)
@@ -9,12 +25,13 @@
 #   - add '-o -name ...' to include any other filenames
 #   - remove '-maxdepth 0' if you want to search recursively
 #   - modify '! -name ...' to exclude any filenames
-
-cgrep() {
+#
+# Usage:
+#   cgrepall [PATTERN]
+cgrepall() {
   if [ -z "$1" ] || [ -n "$2" ]; then
-    echo "Error: Unknown argument"
-    echo "Syntax: cgrep [pattern]"
-    exit 0
+    echo "Usage: cgrepall [pattern]"
+    return 1
   fi
 
   find * -maxdepth 0 -type f \( -name '*.h' -o -name '*.cpp' \) ! -name 'v_*' -exec \
