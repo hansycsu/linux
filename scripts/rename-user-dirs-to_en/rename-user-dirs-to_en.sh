4d41467f7184fe7f0135769e27b6e2434ade906b
YC Su <hansycsu@gmail.com>
2021-06-30 16:11:44 +0800

    Add script: rename-user-dirs

---
+++ scripts/rename-user-dirs-to_en/rename-user-dirs-to_en.sh
@@ -0,0 +1,11 @@
+mv ~/桌面 ~/Desktop
+mv ~/下載 ~/Download
+mv ~/模板 ~/Templates
+mv ~/公共 ~/Publicshare
+mv ~/文件 ~/Documents
+mv ~/音樂 ~/Music
+mv ~/圖片 ~/Pictures
+mv ~/影片 ~/Videos
+mv ~/.config/user-dirs.dirs ~/.config/user-dirs.dirs.bak
+cp user-dirs.dirs ~/.config/user-dirs.dirs
+reboot
